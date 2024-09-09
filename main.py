e9da0112b4a76f5e2f71ac9c8bbc424c26e27b6e
Elosajonara <sawicki5000@gmail.com>
2024-09-09 16:52:08 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,3 @@
+import random
+faktyomnie = ["moje hobby to zwiedzanie rużnych miejsc", "nie mam zwierzątka", "jestem najbardziej dumny z gry kturą stworze", "¯moje ulubione danie to kaczka"]
+print(random.choice(faktyomnie))
